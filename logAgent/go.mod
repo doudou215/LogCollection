58753a43863f726ecd91269dc0a84c77d7c7f506
Zhang YongLiang <dream20151224@163.com>
2021-04-14 12:41:22 +0800

    fix tailObj not used error

--- logAgent/go.mod
+++
@@ -1,3 +0,0 @@
-module logAgent
-
-go 1.15
--- logAgent/tailLog/tailLog.go
+++ logAgent/tailLog/tailLog.go
@@ -1,22 +1,25 @@
 package tailLog
 
 import (
+	"fmt"
 	"github.com/hpcloud/tail"
 )
 
 var (
 	tailObj *tail.Tail
 )
+
 func Init(filename string) error {
 	config := tail.Config{
-		ReOpen: true,
-		Follow: true,
-		Location: &tail.SeekInfo{Offset: 0, Whence: 0},
+		ReOpen:    true,
+		Follow:    true,
+		Location:  &tail.SeekInfo{Offset: 0, Whence: 0},
 		MustExist: false,
-		Poll: true,
+		Poll:      true,
 	}
 
 	tailObj, err := tail.TailFile(filename, config)
+	fmt.Println(tailObj.Filename)
 	if err != nil {
 		return err
 	}
